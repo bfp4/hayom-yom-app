17fe42e8ae7a1d7cbbe32aa1002e961d39b87060
bfp4 <66973969+bfp4@users.noreply.github.com>
2021-06-22 22:13:12 -0400

    update

--- components/body/Body.js
+++ components/body/Body.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { View, useWindowDimensions } from 'react-native';
+import { View } from 'react-native';
 import { connect } from "react-redux"
 import { ScrollView, HayomYomCon, EnglishText, HebrewText } from "./styles"
 
